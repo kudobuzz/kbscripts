7109a63ed90c678dd8d0d77acbcccb398b3302e5
obengwilliam2 <iamkojoobeng@gmail.com>
2020-12-14 11:24:20 +0100

    ch(*): bump version

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -1,29 +1,28 @@
-'use strict'
+"use strict";
 module.exports = {
   extends: [
-    require.resolve('eslint-config-standard'),
-    'plugin:security/recommended',
-    'prettier'
+    require.resolve("eslint-config-standard"),
+    "plugin:security/recommended",
   ],
-  plugins: ['security', 'mocha', 'no-loops', 'this'],
+  plugins: ["security", "mocha", "no-loops", "this"],
   rules: {
-    strict: ['error'],
-    'this/no-this': 'error',
-    'no-loops/no-loops': 2,
-    'mocha/handle-done-callback': 'error',
-    'mocha/max-top-level-suites': ['warn', { limit: 2 }],
-    'mocha/no-identical-title': 'error',
-    'mocha/no-synchronous-tests': 'error',
-    'prefer-arrow-callback': 0,
-    'mocha/valid-suite-description': ['warn', '^[A-Z]'],
-    'mocha/valid-test-description': ['warn', '^[a-z]'],
-    'mocha/prefer-arrow-callback': 2,
-    'mocha/no-return-and-callback': 'error'
+    strict: ["error"],
+    "this/no-this": "error",
+    "no-loops/no-loops": 2,
+    "mocha/handle-done-callback": "error",
+    "mocha/max-top-level-suites": ["warn", { limit: 2 }],
+    "mocha/no-identical-title": "error",
+    "mocha/no-synchronous-tests": "error",
+    "prefer-arrow-callback": 0,
+    "mocha/valid-suite-description": ["warn", "^[A-Z]"],
+    "mocha/valid-test-description": ["warn", "^[a-z]"],
+    "mocha/prefer-arrow-callback": 2,
+    "mocha/no-return-and-callback": "error",
   },
   parserOptions: {
-    sourceType: 'script'
+    sourceType: "script",
   },
   env: {
-    mocha: true
-  }
-}
+    mocha: true,
+  },
+};
